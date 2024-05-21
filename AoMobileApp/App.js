3a7ccb64746a56fd0dd3467c2b667c6ecb1f394b
megan.jimenez <megan.jimenez@ibm.com>
2024-05-21 11:17:08 -0400

    first draft

--- AoMobileApp/App.js
+++ AoMobileApp/App.js
@@ -1,34 +1,79 @@
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+const HomeScreen = ({navigator}) => {
+  const Tab = createBottomTabNavigator();
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
-    </View>
-  );
-}
-
-function Emotion_Wheel_Screen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
-    {/* TODO we should seperate these to style sheet and probably seperate file for each screen as well */}
-      <Text>Emotion Wheel!</Text>
-      <Text>Add image or clickable objects</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Emotion Wheel" component={Emotion_Wheel_Screen} />
       </Tab.Navigator>
+    </View>
+  );
+}
+
+const Emotion_Wheel_Screen = ({navigator}) =>  {
+  const Tab = createBottomTabNavigator();
+  return (
+    <View>
+      <Text> Emotion Wheel </Text>
+      <Text> Instruction/General Info </Text>
+      <Button title="EmotionOne" onPress={() => navigator.push('Emotion1')}/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Emotion Wheel" component={Emotion_Wheel_Screen} />
+      </Tab.Navigator>
+    </View>
+    </View>
+      
+  );
+}
+
+
+const Emotion1 = ({navigator}) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+      <Text>Emotion Wheel Ring 1!</Text>
+      <Button title="Emotion" onPress={() => navigator.push('Emotion2')}/>
+    </View>
+  );
+}
+
+const Emotion2 = ({navigator}) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+      <Text>Emotion Wheel Ring 2!</Text>
+      <Button title="Emotion" onPress={() => navigator.push('Result')}/>
+    </View>
+  );
+}
+
+const Result = ({navigator}) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+      <Text>This is your emotion result.</Text>
+    </View>
+  );
+}
+
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator title="root" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="EmotionWheelHome" component={Emotion_Wheel_Screen} />
+        <Stack.Screen name="EmotionOne" component={Emotion1} />
+        <Stack.Screen name="EmotionTwo" component={Emotion2} />
+        <Stack.Screen name="Result" component={Result} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
