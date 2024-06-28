536a9609cc898cc79a175c940e78a3d9a96b5db8
Lerenzo-Martinez <lerenzo.martinez@ibm.com>
2024-06-28 15:41:21 -0500

    added index.tsx to fx 'Unmatched Route' error. App should now open to home tab

---
+++ AoMobileApp/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import { Redirect } from "expo-router";
+
+
+// export default function Page() {
+//     return <Text>Home Page</Text>;
+// }
+
+const Index = () => {
+    return <Redirect href="/AONest"/>;
+};
+
+export default Index;
