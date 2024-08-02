68b7454534a68065347260539cc805e1a3cb94e3
Michael-Karb <michael.karb@ibm.com>
2024-08-02 15:04:48 -0400

    fix "undefined" path issue on emotion wheel L3

--- AoMobileApp/app/EmotionWheel/[secondary]/[tertiary]/index.js
+++ AoMobileApp/app/EmotionWheel/[secondary]/[tertiary]/index.js
@@ -12,10 +12,14 @@ export default function EmotionWheelLevel3() {
   const { tertiary } = useLocalSearchParams();
   const full_emotion = tertiary ? emotions.find(el => el.secondary.find(innerEl => innerEl.text === tertiary) ) : {};
   const emotion = full_emotion.secondary.find(el => el.text === tertiary)
+
+  const { secondary } = useLocalSearchParams();
+  const prev_emotion = secondary ? emotions.find(el => el.text === secondary) : {};
+
   return(
     <PageWrapper>
       <Text style={styles.subtitle}>Select a more specific emotion</Text>
-      <Emotions emotions={emotion?.tertiary || []} baseHref={`/EmotionWheel/${emotion.from}/${emotion.text}/`} />
+      <Emotions emotions={emotion?.tertiary || []} baseHref={`/EmotionWheel/${prev_emotion.text}/${emotion.text}/`} />
     </PageWrapper>
   );
 };
