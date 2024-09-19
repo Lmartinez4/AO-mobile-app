d2221cf786b59a91082d61e68816aeec428be904
Lerenzo-Martinez <lerenzo.martinez@ibm.com>
2024-09-19 12:23:16 -0500

    attemping to update package-lock.json changes

--- AoMobileApp/app/EmotionWheel/[secondary]/[tertiary]/[selection].js
+++ AoMobileApp/app/EmotionWheel/[secondary]/[tertiary]/[selection].js
@@ -52,7 +52,7 @@ export default function EmotionWheelSelection() {
           <View style={{flex: 0.75, alignItems: 'center'}}>
             <Text style={styles.title}>Emotional Intelligence</Text>
             <Text style={styles.subtitle}>You selected {selection}.</Text>
-            <Link href="/AONest/content">See available content here</Link> 
+            <Link href="/AONest/content">See available content here</Link>
             <WebView
               style={{
                 top: '10%',
