4e5f9f5720f8b3f1182d8832820b1a7c5f52cc96
Lerenzo-Martinez <lerenzo.martinez@ibm.com>
2024-09-19 15:55:55 -0500

    Added Happy, Bad, & Disgust to emotion map

--- AoMobileApp/app/constants/emotions.js
+++ AoMobileApp/app/constants/emotions.js
@@ -62,17 +62,239 @@ export default [{
   text: 'Happy',
   emoji: '0x1F600',
   video: 'https://play.vidyard.com/w6sJFLjJyt6jwHHF17qrjQ.html?',
-  secondary: []
+  secondary: [{
+    text: 'Accepted',
+    emoji: '',
+    tertiary: [
+      {
+        text: 'Respected',
+        emoji: '',
+      },
+      {
+        text: "Valued",
+        emoji: ''
+      }
+    ]
+  }, {
+    text: 'Content',
+    emoji: '',
+    tertiary: [
+      {
+        text: 'Free',
+        emoji: ''
+      },
+      {
+        text: 'Joyful',
+        emoji: ''
+      }
+    ]
+  }, {
+    text: "Interested",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Curious",
+        emoji: ''
+      },
+      {
+        text: "Inquisitive",
+        emoji: ''
+      }
+    ]
+    }, {
+      text: "Optimistic",
+      emoji: '',
+      tertiary: [
+        {
+          text: "Hopeful",
+          emoji: ""
+        },
+        {
+          text: 'Inspired',
+          emoji: ""
+        }
+      ]
+    }, {
+      text: "Peaceful",
+      emoji: "",
+      tertiary: [
+        {
+          text: "Loving",
+          emoji: ""
+        },
+        {
+          text: "Thankful",
+          emoji: ""
+        }
+      ]
+    }, {
+      text: "Playful",
+      emoji: "",
+      tertiary: [
+        {
+          text: "Aroused",
+          emoji: ""
+        },
+        {
+          text: "Cheeky",
+          emoji: ""
+        }
+      ]
+    }, {
+      text: "Powerful",
+      emoji: "",
+      tertiary: [
+        {
+          text: "Courageous",
+          emoji: ""
+        },
+        {
+          text: "Creative",
+          emoji: ""
+        }
+      ]
+    }, {
+      text: "Proud",
+      emoji: "",
+      tertiary: [
+        {
+          text: "Confident",
+          emoji: ""
+        },
+        {
+          text: "Successful",
+          emoji: ""
+        }
+      ]
+    }, {
+      text: "Trusting",
+      emoji: "",
+      tertiary: [
+        {
+          text: "Intimate",
+          emoji: ""
+        },
+        {
+          text: "Sensitive",
+          emoji: ""
+        }
+      ]
+    }]
 }, {
   text: 'Bad',
   emoji: '0x1F61F',
   video: 'https://play.vidyard.com/FWfbB6mxHW4nzJb43WZbTb.html?',
-  secondary: []
+  secondary: [{
+    text: "Bored",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Apathetic",
+        emoji: ""
+      },
+      {
+        text: "Indifferent",
+        emoji: ""
+      }
+    ]
+  },{
+    text: "Busy",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Pressured",
+        emoji: ""
+      },
+      {
+        text: "Rushed",
+        emoji: ""
+      }
+    ]
+  }, {
+    text: "Stressed",
+    emoji: "",
+    tertiary: [
+      {
+        //banner adds % to spaces to header near "back" on top of page.
+        text: "Out of Control",
+        emoji: ""
+      },
+      {
+        text: "Overwhelmed",
+        emoji: ""
+      }
+    ]
+  }, {
+    text: "Tired",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Sleepy",
+        emoji: ""
+      },
+      {
+        text: "Unfocused",
+        emoji: ""
+      }
+    ]
+  }]
 }, {
   text: 'Disgust',
   emoji: '0x1F922',
   video: 'https://play.vidyard.com/o7dLbYEUJ2RJf7tZGYs7Pm.html?',
-  secondary: []
+  secondary: [{
+    text: "Awful",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Detestable",
+        emoji: ""
+      },
+      {
+        text: "Nauseated",
+        emoji: ""
+      }
+    ]
+  }, {
+    text: "Disappointed",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Appalled",
+        emoji: ""
+      },
+      {
+        text: "Revolted",
+        emoji: ""
+      }
+    ]
+  }, {
+    text: "Disapproving",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Embarrased",
+        emoji: "",
+      },
+      {
+        text: "Judgemental",
+        emoji: ""
+      }
+    ]
+  }, {
+    text: "Repelled",
+    emoji: "",
+    tertiary: [
+      {
+        text: "Hesitant",
+        emoji: ""
+      },
+      {
+        text: "Horrified",
+        emoji: ""
+      }
+    ]
+  }]
 }, {
   text: 'Fearful',
   emoji: '0x1F628',
